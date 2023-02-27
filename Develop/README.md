bb67356a65f96c44a3e3ddadb53b683e6209c84d
fardinatabassum <fardinatabassum@gmail.com>
2023-02-27 17:06:29 -0500

    fixed license badge and link section

--- Develop/README.md
+++ Develop/README.md
@@ -1,42 +1,45 @@
 
-# 
-
-
-## Table of Contents
-- [Description](#description)
-- [Installation](#installation)
-- [Credits](#credits)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Description
-
-
-
-## Installation
-
-
-
-## Credits
-
-
-
-## License
-
-## Contributing
-
-
-
-## Tests
-
-
-
-## Questions
-
-https://github.com/fardinatabassum
-
-'For further inquiries reach me at '
-
-
+  # 
+  # [![License MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Description
+  
+  
+  
+  ## Installation
+  
+  
+  
+  ## Credits
+  
+  
+  
+  ## License
+  Uses the [MIT](https://choosealicense.com/licenses/mit/) license
+  
+  ## Contributing
+  
+  
+  
+  ## Tests
+  
+  
+  
+  ## Questions
+  
+  https://github.com/
+  
+  For further inquiries reach me at 
+  
+  ### Live Site: https://gituhb.com//
+  
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,92 +1,98 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(data) {
-  const badges = {
-    MIT: '[![License MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    Apache: '[Apache License 2.0](https://img.shields.io/badge/License-Apache 2.0-green.svg)(https://opensource.org/license/apache-2-0/)',
-    Mozilla: '[Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla 2.0-green.svg)(https://opensource.org/license/mpl-2-0/)',
-    ISC: '[ISC](https://img.shields.io/badge/License-ISC-blue.svg)(https://choosealicense.com/licenses/isc/)'
-
+function renderLicenseBadge(license ) {
+  let badge = ''
+  if (license == 'MIT License'){
+    badge = '[![License MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }else if (license == 'Apache License 2.0'){
+    badge = '[![Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-green.svg)](https://opensource.org/license/apache-2-0/)'
+  }else if (license == 'Mozilla Public License 2.0'){
+    badge = '[![Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla_2.0-green.svg)](https://opensource.org/license/mpl-2-0/)'
+  }else if (license == "ISC"){
+    badge = '[![ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://choosealicense.com/licenses/isc/)'
+  }else{
+    badge = ''
   }
-  return badges[data]
-  } 
-  // check if data.license is none, return nothing, else return the badge needed
+  return badge
+} 
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(data) { 
-  const licenseLinks = {
-    MIT: '[MIT](https://choosealicense.com/licenses/mit/)',
-    Apache: '[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)',
-    Mozilla: '[Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)',
-    ISC: '[ISC](https://choosealicense.com/licenses/isc/)'
-
+function renderLicenseLink(license) {
+  let licenseLink = '' 
+  if (license == 'MIT License'){
+    licenseLink = '[MIT](https://choosealicense.com/licenses/mit/)'
+  }else if (license == 'Apache License 2.0'){
+    licenseLink = '[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)'
+  }else if (license == 'Mozilla Public License 2.0'){
+    licenseLink = '[Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)'
+  }else if (license == "ISC"){
+    licenseLink = '[ISC](https://choosealicense.com/licenses/isc/)'
+  }else{
+    licenseLink = ''
   }
-  return licenseLinks[data]
-  //
+  return licenseLink
+
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(data) {
-  if(license === "None") {
-    return '';
-  } else {
-    // return `${this.renderLicenseLink(data)}`
-    return `# License ${renderLicenseBadge(data)}`
-  }
-}
+// function renderLicenseSection(license) {
+//   if(data.license === "None") {
+//     return "" ;
+//   } else {
+//     return `## License
+//     ${renderLicenseBadge(license)}`
+//   }
+// }
 
 
 
-// ${renderLicenseSection(data.license)}
-// # ${this.renderLicenseBadge(data)}
-/// ### Live Site: https://gituhb.com/${data.github}/${data.title}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
-
-## Table of Contents
-- [Description](#description)
-- [Installation](#installation)
-- [Credits](#credits)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Description
-
-${data.description}
-
-## Installation
-
-${data.installation}
-
-## Credits
-
-${data.credits}
-
-## License
-
-## Contributing
-
-${data.contributing}
-
-## Tests
-
-${data.tests}
-
-## Questions
-
-https://github.com/${data.github}
-
-'For further inquiries reach me at ${data.email}'
-
-
-`;
+  # ${data.title}
+  # ${renderLicenseBadge(data.license)}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+  Uses the ${renderLicenseLink(data.license)} license
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  https://github.com/${data.github}
+  
+  For further inquiries reach me at ${data.email}
+  
+  ### Live Site: https://gituhb.com/${data.github}/${data.title}
+  
+  `;
 }
 
 module.exports = generateMarkdown;
